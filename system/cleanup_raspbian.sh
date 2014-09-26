7e31bef12e19de0ee5fd8b7e14c644c1f9d6e170
AlexFaraino <AlexFaraino@users.noreply.github.com>
2014-09-26 14:36:07 +0200

    fix auto install

--- system/cleanup_raspbian.sh
+++ system/cleanup_raspbian.sh
@@ -2,16 +2,16 @@
 # Launch as root
 
 # Remove useless packages
-apt-get purge x11-common midori lxde dillo gnome-icon-theme \
+apt-get --yes purge x11-common midori lxde dillo gnome-icon-theme \
   gnome-themes-standard-data libgnome-keyring-common libgnome-keyring0 \
   libsoup-gnome2.4-1 lxde-common lxde-icon-theme omxplayer dbus-x11 libx11-6 \
   libx11-data libx11-xcb1 gcc-4.5-base gcc-4.6-base desktop-file-utils \
   debian-reference-en debian-reference-common java-common
 
 # Remove unused packets
-apt-get autoremove --purge
+apt-get --yes autoremove --purge
 
 # Clear APT cache
-apt-get clean
+#apt-get clean
 
 rm -rf /opt/vc
