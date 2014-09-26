d92df66ff52a13ab12168737c6eff2c78d37a59a
Phyks <phyks@phyks.me>
2014-09-26 18:14:33 +0000

    Improve startup script

--- receive/startup.sh
+++ receive/startup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 echo "Starting receive script…"
-screen -dmS receive && screen -S receive -p 0 -X stuff "./receive$(printf \\r)"
+screen -dmS receive && screen -S receive -p 0 -X stuff "while true; do ./receive; done$(printf \\r)"
 echo "Done !\n"
 sleep 0.2
 echo "Starting processing script…"
-screen -dmS process && screen -S process -p 0 -X stuff "python3 process.py$(printf \\r)"
+screen -dmS process && screen -S process -p 0 -X stuff "while true; do python3 process.py; done$(printf \\r)"
 echo "Done !\n"
 
 echo "Ready to start !"
