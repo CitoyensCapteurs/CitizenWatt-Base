30e6073ef119d91b9a3ae45239b39a31944256f6
AlexFaraino <AlexFaraino@users.noreply.github.com>
2014-09-28 21:53:06 +0200

    keep gcc

--- system/cleanup_raspbian.sh
+++ system/cleanup_raspbian.sh
@@ -2,11 +2,11 @@
 # Launch as root
 
 # Remove useless packages
-apt-get --yes purge x11-common midori lxde dillo gnome-icon-theme \
+apt-get --yes purge x11-common lxde dillo gnome-icon-theme \
   gnome-themes-standard-data libgnome-keyring-common libgnome-keyring0 \
   libsoup-gnome2.4-1 lxde-common lxde-icon-theme omxplayer dbus-x11 libx11-6 \
-  libx11-data libx11-xcb1 gcc-4.5-base gcc-4.6-base desktop-file-utils \
-  debian-reference-en debian-reference-common java-common
+  libx11-data libx11-xcb1 desktop-file-utils debian-reference-en \
+  debian-reference-common java-common
 
 # Remove unused packets
 apt-get --yes autoremove --purge
@@ -14,4 +14,4 @@ apt-get --yes autoremove --purge
 # Clear APT cache
 #apt-get clean
 
-rm -rf /opt/vc
+rm -rf /opt/vc /home/pi/Desktop /home/pi/python_games /home/pi/ocr_pi.png
