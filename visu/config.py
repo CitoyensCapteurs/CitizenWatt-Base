72b71c2984bcb460b8a7b7604d5e51db6ebe139d
Phyks <webmaster@phyks.me>
2014-09-28 21:41:07 +0200

    Config script

---
+++ visu/config.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+import os
+import errno
+import json
+import sys
+import tools
+
+
+def make_sure_path_exists(path):
+    try:
+        os.makedirs(path)
+        return False
+    except OSError as exception:
+        if exception.errno != errno.EEXIST:
+            raise
+        else:
+            return True
+
+
+class Config():
+    def __init__(self, base_config_path="~/.config/citizenwatt/"):
+        self.config_path = os.path.expanduser(base_config_path)
+        self.config = {}
+        self.load()
+
+    def as_dict(self):
+        return self.config
+
+    def get(self, param):
+        return self.config.get(param, False)
+
+    def set(self, param, value):
+        self.config[param] = value
+
+    def initialize(self):
+        self.set("ignore_fields", ["file", "doi", "tag"])
+        self.save()
+
+    def load(self):
+        try:
+            folder_exists = make_sure_path_exists(self.config_path)
+            if(folder_exists and
+               os.path.isfile(self.config_path + "config.json")):
+                initialized = True
+            else:
+                initialized = False
+        except OSError:
+            tools.warning("Unable to create ~/.config folder.")
+            sys.exit(1)
+        if not initialized:
+            self.initialize()
+        else:
+            try:
+                with open(self.config_path + "config.json", 'r') as fh:
+                    self.config = json.load(fh)
+            except (ValueError, IOError):
+                tools.warning("Config file could not be read.")
+                sys.exit(1)
+
+    def save(self):
+        try:
+            with open(self.config_path + "config.json", 'w') as fh:
+                fh.write(json.dumps(self.config))
+        except IOError:
+            tools.warning("Could not write config file.")
+            sys.exit(1)
---
+++ visu/tools.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+import sys
+
+
+def warning(*objs):
+    """Write warnings to stderr"""
+    print("WARNING: ", *objs, file=sys.stderr)
