69c40d77f06df5997261187f9f40f6c7d3d20b34
Phyks <webmaster@phyks.me>
2014-09-30 00:48:44 +0200

    Fix citizenwatt.sh

--- system/citizenwatt.sh
+++ system/citizenwatt.sh
@@ -12,14 +12,14 @@
 #!/bin/sh
 
 echo "Starting the webserver…"
-screen -dmS visu && screen -S visu -p 0 -X stuff "cd visu; while true; do python3 visu.py; done$(printf \\r)"
+screen -dmS visu && screen -S visu -p 0 -X stuff "while true; do python3 visu.py; done$(printf \\r)"
 
 echo "Starting receive script…"
-screen -dmS receive && screen -S receive -p 0 -X stuff "cd receive; while true; do ./receive; done$(printf \\r)"
+screen -dmS receive && screen -S receive -p 0 -X stuff "while true; do ./receive; done$(printf \\r)"
 echo "Done !\n"
 sleep 0.2
 echo "Starting processing script…"
-screen -dmS process && screen -S process -p 0 -X stuff "cd receive; while true; do python3 process.py; done$(printf \\r)"
+screen -dmS process && screen -S process -p 0 -X stuff "while true; do python3 process.py; done$(printf \\r)"
 echo "Done !\n"
 
 while ! curl -s --head http://localhost:8080 2>&1 > /dev/null; do
