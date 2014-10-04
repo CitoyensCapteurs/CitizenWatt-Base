a8df43121c0c6a529b4c7e498705657234a4ff2a
Élie Michel <elie.michel@ens.fr>
2014-10-04 12:12:03 +0200

    Add rotation to loading image in Community
    And remove hack in App.js to see previous day.

--- static/js/conso/App.js
+++ static/js/conso/App.js
@@ -119,8 +119,8 @@ var App = function() {
 			case 'day':
 				target
 				+= '/by_time/'
-				+  (dateutils.getDayStart() / 1000.0 - 86400) + '/'
-				+  (dateutils.getDayEnd() / 1000.0 - 86400) + '/'
+				+  dateutils.getDayStart() / 1000.0 + '/'
+				+  dateutils.getDayEnd() / 1000.0 + '/'
 				+  dateutils.getHourLength() / 1000.0;
 				graph.setOverviewLabel('Consommation aujourd\'hui');
 				break;
