87e627458eb22a46dcd6402116a5de03236564ad
Alexandre Faraino <AlexFaraino@users.noreply.github.com>
2014-10-21 16:11:46 +0200

    Update updater.sh

--- updater.sh
+++ updater.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-echo "Start update"
+# Stop on any error
+set -e
+
+echo "Start CitizenWatt update..."
 sudo apt-get update
 sudo apt-get --yes --only-upgrade install libnrf24-dev citizenwatt-visu
-echo "End update"
+echo "End CitizenWatt update"
